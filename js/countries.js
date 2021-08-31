02f16e71799b967e18c6532ed091963a7daee9a5
Sunny <shubelislam.diu@gmail.com>
2021-08-31 07:51:07 +0600

    country api

--- js/countries.js
+++ js/countries.js
@@ -16,12 +16,35 @@ const displayCountries = countries => {
     countries.forEach(country => {
         const div = document.createElement("div");
         div.classList.add("country");
-        const h3 = document.createElement("h3");
+        /* const h3 = document.createElement("h3");
         h3.innerText = ` Country: ${country.name} `;
         div.appendChild(h3);
         const p = document.createElement("p");
         p.innerText = ` Capital: ${country.capital} `;
-        div.appendChild(p);
+        div.appendChild(p); */
+        div.innerHTML = `
+            <h3>${country.name}</h3>
+            <p>${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `;
         countriesID.appendChild(div);
     });
+};
+
+const loadCountryByName = countryDetails => {
+    const url = `https://restcountries.eu/rest/v2/name/${countryDetails}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]));
+};
+
+const displayCountryDetails = country => {
+    // console.log(country);
+    const countryDiv = document.getElementById("country-details");
+    countryDiv.innerHTML = `
+        <h5>Name: ${country.name}</h5>
+        <p>Population: ${country.population}</p>
+        <img width="200px" src="${country.flag}">
+    `;
 };
